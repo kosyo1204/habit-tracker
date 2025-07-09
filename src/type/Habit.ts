888ac547beb06ec99b38e7d77fcde63c9baf8048
kosyo1204 <k.soshi1219+github@gmail.com>
2025-07-10 00:02:22 +0900

    インデント修正

--- src/type/Habit.ts
+++ src/type/Habit.ts
@@ -1,19 +1,19 @@
 interface HabitGoal {
-    amount: number
-    unit: 'times' | 'minutes'
+  amount: number
+  unit: 'times' | 'minutes'
 }
 
 export interface Habit {
-    id: string
-    name: string
-    type: 'count' | 'check'
-    goal: HabitGoal
-    frequency: 'daily' | 'weekly' | 'monthly'
-    startAt: string
-    endAt: string
-    createdAt: string
-    updatedAt: string
+  id: string
+  name: string
+  type: 'count' | 'check'
+  goal: HabitGoal
+  frequency: 'daily' | 'weekly' | 'monthly'
+  startAt: string
+  endAt: string
+  createdAt: string
+  updatedAt: string
 
-    value: number | boolean
-    skipped?: boolean
+  value: number | boolean
+  skipped?: boolean
 }
